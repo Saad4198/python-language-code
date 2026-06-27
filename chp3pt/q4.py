1c9495b647bc9945553edf904c4ce483abf70df6
Muhammad Saad <muhsad695@gmail.com>
2026-06-27 18:28:34 +0500

    Add files via upload

---
+++ chp3pt/q4.py
@@ -0,0 +1,7 @@
+name= "Hello how are you  i hope you are fine"
+print(name) #name string value not change this is called immutable means not change orignal string
+print(name.replace("  ", " "))  #this is replace the double space into the single space of the name string
+value="12 34 56 77 8990"
+print(value)
+print(value.replace("56","786") .replace("77", "56"))
+ #they change the value of string 56 convert 786 and 77 convert it into 56
---
+++ chp3pt/q5.py
@@ -0,0 +1,7 @@
+"""
+this q problem who to used esacpe sequence and benefit of escape sequence in wrtie data and i am data by using  and without escape sequence.
+"""
+value="HLO1! saad how are you and you are learn python language course and we used escape sequence character"
+print("before escape sequence value\n",value)
+value="HLO1!\n Saad \t how are you and you are learn python language course\n and we used escape sequence character"
+print("\nAfter escape sequence value\n",value)
